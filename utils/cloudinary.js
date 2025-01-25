bc8af1a37f592cdf7557b1ca66e47873bc3b1461
Tijo <hamsen3880@outlook.com>
2025-01-26 01:33:00 +0530

    bug fixes

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,7 @@
-import {v1 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import dotenv from 'dotenv'
 
-dotenv.config({})
+dotenv.config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
